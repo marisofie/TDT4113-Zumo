396aa7517fac13003e1d37633acc28b8edcd2c23
marisofie <marilerfaldet@gmail.com>
2017-11-05 14:58:37 +0100

    class motob
    class motob, can drive left, right, forward, backward, stop and rotate

---
+++ Motob.py
@@ -0,0 +1,33 @@
+import basic_robot.motors as motors
+
+class Motob:
+
+    def __init__(self):
+        self.motors = motors.Motors()
+        self.value = None
+
+    #updates the motors value and sends direction further
+    def update_motor(self, value):
+        self.value = value
+        self.operationalize()
+
+    #takes a recommendation and makes the motors do the operation.
+    def operationalize(self):
+        for recom in self.value:
+            if recom[0] == 'F':
+                self.motors.forward(speed=recom[1]*0.01) #multiply with 0.01 to get percent
+            elif recom[0] == 'B':
+                motors.backward(speed=recom[1]*0.01)
+            elif recom[0] == 'L':
+                motors.left(speed=recom[1]*0.01)
+            elif recom[0] == 'R':
+                motors.right(speed=recom[1]*0.01)
+            elif recom[0] == 'S':
+                motors.stop()
+
+    #makes the robot rotate, vector [-1, 1], [L, R].
+    def rotate(self, vector):
+        self.motors.set_value(vector)
+
+
+
