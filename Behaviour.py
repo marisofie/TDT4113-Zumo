d0d4eb65d8996b6a67d2771e38289c3f29732f82
marisofie <marilerfaldet@gmail.com>
2017-11-09 15:10:57 +0100

    updated with StopRed and DriveAround

--- Behaviour.py
+++ Behaviour.py
@@ -1,3 +1,5 @@
+import random
+
 class Behavior():
 
     def __init__(self, bbcon, sensobs, priority, active_flag):
@@ -50,4 +52,62 @@ class Behavior():
         if self.active_flag:
             self.sense_and_act()
             self.set_weight()
+
+
+class StopRed(Behavior):
+
+    def __init__(self, priority, active_flag, bbcon=None, sensobs = []):
+        super().__init__(bbcon, sensobs=sensobs, priority=priority, active_flag=active_flag)
+        self.stop_distance = 5
+        self.min_red = 0.3
+        self.motor_recommendations = []
+
+    def consider_activation(self):
+        # if object is closer than 5cm
+        # must remember that sensobs[0] contains distance
+        percent_red = self.sensobs[1].get_sensor_value()
+        if self.sensobs[0].value <= self.stop_distance and percent_red > self.min_red:
+            self.active_flag = True
+
+    def consider_deactivation(self):
+        # if object is farther away than 5cm, deactivates behaviour
+        percent_red = self.sensobs[1].get_sensor_value()
+        if self.sensobs[0].get_sensor_value() > self.stop_distance or percent_red < self.min_red:
+            self.active_flag = False
+            self.match_degree = 0
+            self.motor_recommendations = []
+
+    def sense_and_act(self):
+        percent_red = self.sensobs[1].get_sensor_value()
+        self.match_degree = percent_red
+        self.motor_recommendations = [('S', 0)]
+
+
+# makes the robot drive around until sensors get something.
+class DriveAround(Behavior):
+
+    def __init__(self, priority, active_flag, bbcon=None, sensobs = []):
+        super().__init__(bbcon, sensobs=sensobs, priority=priority, active_flag = active_flag)
+
+
+    def consider_deactivation(self):
+        self.active_flag = True
+        self.match_degree = 0.1
+
+    def consider_activation(self):
+        self.active_flag = True
+        self.match_degree = 0.1
+
+    def sense_and_act(self):
+        directions = ['R', 'L', 'F', 'B']
+        direction = random.randint(0, 3)
+        speed = random.randint(0, 100)
+        self.motor_recommendations[(direction, speed)]
+
+
+
+
+
+
+
 
